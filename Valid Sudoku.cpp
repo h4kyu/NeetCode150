e471642b30cddf648cf64073f86d4b58ead5a365
Nahshon Weissberg <nahshonweissberg@gmail.com>
2025-05-13 13:12:42 -0400

    Valid Sudoku; done

--- Valid Sudoku.cpp
+++ Valid Sudoku.cpp
@@ -9,5 +9,31 @@ public:
                     --> if no, insert to respective sets and continue to next char
             after all chars passed, return true.
         */
+        unordered_map<int, unordered_set<char>> rows, cols;
+        map<pair<int, int>, unordered_set<char>> boxes;
+
+        for (int row{0}; row < 9; ++row) {
+            for (int col{0}; col < 9; ++col) {
+                char number{board[row][col]};
+                if (number == '.') continue;
+
+                // check for rows and columns
+                if (rows[row].count(number) || cols[col].count(number)) {
+                    return false;
+                }
+
+                // check for boxes
+                pair<int, int> boxNumber = {row/3, col/3};
+                if (boxes[boxNumber].count(number)) {
+                    return false;
+                }
+
+                // insert to respective sets
+                rows[row].insert(number);
+                cols[col].insert(number);
+                boxes[boxNumber].insert(number);
+            }
+        }
+        return true;
     }
 };
