bbb3f503302a96ddd563a11edb9a6f6cda767cb3
Nahshon Weissberg <nahshonweissberg@gmail.com>
2025-05-28 22:21:34 -0400

    3Sum; revised pseudocode

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -2,30 +2,29 @@ class Solution {
 public:
     /*
     sort nums in increasing order.
-    initialize 3 pointers — 
+    initialize 2 pointers — 
     lower at index 0, 
-    middle at index 1, 
-    upper at index n.
+    upper at index n-1.
     initialize vector<vector<int>> triplets
-    1. increment upper downward
-    while upper > lower, 
-    if nums[upper] + nums[lower] + nums[middle] < 0
-        ++middle
-    else if nums[upper] + nums[lower] + nums[middle] = 0
-        add [lower, middle, upper] to triplets
-    else
-        --upper
-        middle = lower + 1
-    
-    2. increment lower upward
-    while upper > lower, 
-    if nums[upper] + nums[lower] + nums[middle] < 0
-        ++middle
-    else if nums[upper] + nums[lower] + nums[middle] = 0
-        add [lower, middle, upper] to triplets
-    else
-        ++lower
-        middle = lower + 1
+    for i=0 to n-1
+        lower = i+1
+        if nums[i] > 0
+            break
+        if (i > 0 && nums[i] == nums[i-1]) 
+            continue;
+        while upper > lower
+            sum = nums[i] + nums[lower] + nums[upper]
+            if sum == 0
+                add to triplets
+                --upper
+                ++lower
+                Skip duplicates for lower and upper -->
+                while (lower < upper && nums[lower] == nums[lower - 1]) ++lower;
+                while (lower < upper && nums[upper] == nums[upper + 1]) --upper;
+            else if sum > 0
+                --upper
+            else
+                ++lower
     */
     
     vector<vector<int>> threeSum(vector<int>& nums) {
