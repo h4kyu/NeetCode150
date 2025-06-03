6429e231410e7c5d8699719b9728c0d877b57715
Nahshon Weissberg <nahshonweissberg@gmail.com>
2025-06-03 10:03:47 -0400

    Best Time to Buy and Sell Stock; pseudocode

---
+++ Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    /*
+    sol:
+    int curr
+    int fut
+    int globalMin
+    int globalMax
+    int maxDiff
+    iterate backwards
+        if curr > globalMax
+            globalMax = curr
+        else if curr < globalMin
+            globalMin = curr
+        int currDiff = globalMax - globalMin
+        if currDiff > maxDiff
+            maxDiff = currDiff
+    return maxDiff
+    */
+    int maxProfit(vector<int>& prices) {
+        
+    }
+};
