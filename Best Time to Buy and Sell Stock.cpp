b1934c6ba90502881764cd526cb6f52ff2cf707b
Nahshon Weissberg <nahshonweissberg@gmail.com>
2025-06-03 10:51:35 -0400

    Best Time to Buy and Sell Stock; modified pseudocode

--- Best Time to Buy and Sell Stock.cpp
+++ Best Time to Buy and Sell Stock.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     /*
     sol:
-    int curr
-    int fut
-    int globalMin
-    int globalMax
-    int maxDiff
-    iterate backwards
-        if curr > globalMax
-            globalMax = curr
-        else if curr < globalMin
-            globalMin = curr
-        int currDiff = globalMax - globalMin
-        if currDiff > maxDiff
-            maxDiff = currDiff
-    return maxDiff
+    int profit
+    int maxProfit{0}
+    int sell
+    int buy
+    iterate over prices
+        if s < b
+            b = s
+            ++s
+        else 
+            profit = s - b
+            if profit > maxProfit
+                maxProfit = profit
+    return maxProfit
     */
     int maxProfit(vector<int>& prices) {
         
